f85f0d7cc3b82c917fec98ca37050c048dffeeb0
Todd Albert <todd@bocacode.com>
2022-07-28 15:48:39 -0400

    pushed to firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,17 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 
-exports.helloThere = functions.https.onRequest((req, res) => {
-  res.send('Hello there Firebase Cloud user!');
+const app = express();
+app.use(cors());
+
+// setup routes...
+app.get('/test', (req, res) => {
+  res.send('Wow, it actually worked.');
 });
 
+app.get('/another-one', (req, res) => {
+  res.send('This one works, too!');
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest(app);
