7949ddd0a8213fe0f585caab1953872104fc9315
jonathans199 <jso1919@gmail.com>
2022-07-28 17:11:52 -0400

    updated repo with proper engine

--- functions/index.js
+++ functions/index.js
@@ -14,10 +14,6 @@ app.get('/another-one', (req, res) => {
   res.send('This one works, too!');
 });
 
-app.delete('/delete-one', (req, res) => {
-  Array.splice(3,5) 
-})
-
 exports.api = functions.https.onRequest(app);
 
 // We did it
